b391174ea8eee018d397d24e6ef739380a22e10d
ayushh-gautam <ayushgautam896@gmail.com>
2023-08-23 21:04:11 +0545

    huhuhu

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:chappie/Provider/welcomePageProvider.dart';
 import 'package:chappie/Screens/login&signup/welcomePage.dart';
 import 'package:chappie/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (_) => WelcomePageProvider(),
+      child: MyApp(),
+    )
+  ]));
 }
 
 class MyApp extends StatelessWidget {
